fa438f29e94c091c22fe57025d625a7f8438a2c9
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-20 18:56:30 -0300

    feat: Button to Catalog page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import Carousel from "../components/Carousel/carousel";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <>
       <Carousel />
+      <div className="catalogButton">
+        <Link to={"catalog"}>
+          <button>Catalog</button>
+        </Link>
+      </div>
     </>
   );
 };
