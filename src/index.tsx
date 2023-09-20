ccb8afd46e495b7c30bec287b32d3f17974c0132
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-20 18:40:54 -0300

    feat: Routes to home and article pages.

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from "./pages/Home";
 import Layout from "./components/Layout/layout";
+import Article from "./pages/Article";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ItemProvider } from "./context/sliderItemContext";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "article/:id",
+    element: <Article />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Layout>
-      <Home />
-    </Layout>
+    <ItemProvider>
+      <Layout>
+        <RouterProvider router={router} />
+      </Layout>
+    </ItemProvider>
   </React.StrictMode>
 );
