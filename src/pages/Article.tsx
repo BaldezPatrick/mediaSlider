5ab93e71337dd37853119c326b9cfa34e1493b1d
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-20 18:45:55 -0300

    feat: Accessing the context data to show the infos.

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,7 +1,46 @@
+import { useItemContext } from "../context/sliderItemContext";
+import "./Article.css";
+
 const Article: React.FC = () => {
+  const { id, title, url, thumbnailUrl } = useItemContext();
   return (
     <>
-      <h1>Article</h1>
+      <section className="articleWrapper">
+        <h1>Your selected item details</h1>
+        <div className="articleTable">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <section className="articleImagesWrapper">
+          <h2>Images</h2>
+          <div className="articleImagesItems">
+            <div className="articleImage">
+              <img
+                src={url ? url : "No image content"}
+                alt={url ? url : "No image content info"}
+              />
+            </div>
+            <div className="articleImage">
+              <img
+                src={thumbnailUrl ? thumbnailUrl : "No image content"}
+                alt={thumbnailUrl ? thumbnailUrl : "No image content info"}
+              />
+            </div>
+          </div>
+        </section>
+      </section>
     </>
   );
 };
