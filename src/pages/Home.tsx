b7bcb758c2724afba2b5471d9f2d903fc9000453
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-19 22:49:47 -0300

    feat: Removing the fakeData to implement API calls. SliderItems interface is the expected API data structure.
    To make request, axios is used and only the first 10 items are limited to appear in the list.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,77 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const fakeData = [
-  {
-    id: 1,
-    title: "Item 1",
-    description: "Descrição do Item 1",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 2,
-    title: "Item 2",
-    description: "Descrição do Item 2",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 3,
-    title: "Item 3",
-    description: "Descrição do Item 3",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 4,
-    title: "Item 4",
-    description: "Descrição do Item 4",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 5,
-    title: "Item 5",
-    description: "Descrição do Item 5",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 6,
-    title: "Item 6",
-    description: "Descrição do Item 6",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 7,
-    title: "Item 7",
-    description: "Descrição do Item 7",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 8,
-    title: "Item 8",
-    description: "Descrição do Item 8",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 9,
-    title: "Item 9",
-    description: "Descrição do Item 9",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 10,
-    title: "Item 10",
-    description: "Descrição do Item 10",
-    image: "https://via.placeholder.com/300",
-  },
-  {
-    id: 11,
-    title: "Item 11",
-    description: "Descrição do Item 11",
-    image: "https://via.placeholder.com/300",
-  },
-];
+interface SliderItems {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 
 const Home: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [data, setData] = useState<SliderItems[]>([]);
   const itemsPerPage = 3;
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => {
+        const data: SliderItems[] = res.data.slice(0, 10);
+        setData(data);
+      })
+      .catch((err) => {
+        console.error("API error: ", err);
+      });
+  }, []);
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) =>
@@ -80,13 +33,10 @@ const Home: React.FC = () => {
   };
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % fakeData.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
   };
 
-  const displayedItems = fakeData.slice(
-    currentIndex,
-    currentIndex + itemsPerPage
-  );
+  const displayedItems = data.slice(currentIndex, currentIndex + itemsPerPage);
 
   const limitedDsiplayedItems = displayedItems.filter((item) => item.id <= 10);
 
@@ -101,7 +51,7 @@ const Home: React.FC = () => {
         <div className="carousel-items">
           {limitedDsiplayedItems.map((item) => (
             <article key={item.id} className="carousel-item">
-              <img src={item.image} alt={item.title} />
+              <img src={item.url} alt={item.title} />
               <h2>{item.title}</h2>
             </article>
           ))}
