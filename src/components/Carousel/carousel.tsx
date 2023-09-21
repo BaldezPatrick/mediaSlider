646f9c95c0df2264a9106a738447ebf932129b3d
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-21 16:33:27 -0300

    feat: Get API data and put it together to show in the carousel and also check the screen size to show one or three items.

--- src/components/Carousel/carousel.tsx
+++ src/components/Carousel/carousel.tsx
@@ -15,18 +15,57 @@ interface CarouselItem {
 const Carousel: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [data, setData] = useState<CarouselItem[]>([]);
-  const itemsPerPage = 3;
+  const [itemsPerPage, setItemsPerPage] = useState<number>(3);
 
   useEffect(() => {
     axios
       .get<CarouselItem[]>("https://jsonplaceholder.typicode.com/photos")
       .then((res) => {
-        const items: CarouselItem[] = res.data.slice(0, 10);
-        setData(items);
+        axios
+          .get<CarouselItem[]>("https://jsonplaceholder.typicode.com/posts")
+          .then((resPost) => {
+            const maxItemsOnSlider = window.innerWidth < 768 ? 12 : 10;
+            const combinedItems: CarouselItem[] = res.data
+              .slice(0, maxItemsOnSlider)
+              .map((photoItem) => {
+                const matchingPostItem = resPost.data.find(
+                  (postItem) => postItem.id === photoItem.id
+                );
+                if (matchingPostItem) {
+                  return {
+                    ...photoItem,
+                    title: matchingPostItem.title,
+                    body: matchingPostItem.body,
+                  };
+                } else {
+                  return {
+                    ...photoItem,
+                    body: "",
+                  };
+                }
+              });
+            setData(combinedItems);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
       })
       .catch((err) => {
         console.error("API error: ", err);
       });
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newItemsPerPage = window.innerWidth < 768 ? 1 : 3;
+      setItemsPerPage(newItemsPerPage);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   const handlePrev = () => {
